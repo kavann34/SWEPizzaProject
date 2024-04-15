3bdca7be96e48f6a651e001218b4d79675de9ad9
ColinCPittman <colincpittman@gmail.com>
2024-04-15 16:13:17 -0400

    added an updateAccount method
    Signed-off-by: ColinCPittman <colincpittman@gmail.com>

--- src/main/java/com/projectpizza/projectpizza/Account.java
+++ src/main/java/com/projectpizza/projectpizza/Account.java
@@ -74,7 +74,55 @@ public class Account {
         }
         return "Error";
     }
+
+    /**
+     * Scans the accountDatabase file line by line recording the information, if it reaches a line with
+     * the matching phone number, it replaces the line with the new info in the parameters, then
+     * it continues scanning lines and recording until it reaches the end. Overwrites the database
+     * with the updated version if it found the match, else it does nothing.
+     * @param phone
+     * @param password
+     * @param accountType
+     * @param firstName
+     * @param lastName
+     * @param addressStreet
+     * @param addressCity
+     * @param addressState
+     * @param addressZip
+     * @param email
+     * @return true if update was successful.
+     */
     public static boolean updateAccount(String phone, String password, String accountType, String firstName, String lastName, String addressStreet, String addressCity, String addressState, int addressZip, String email){
-        return true;
+        StringBuilder updatedDatabase = new StringBuilder();
+        boolean updatedResult = false;
+
+        try (Scanner scanner = new Scanner(accountDatabase)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] data = line.split(",");
+
+                if (data[0].equals(phone)) {
+                    line = phone + "," + password + "," + accountType + "," + lastName + "," + addressStreet + "," + addressCity + "," + addressState + "," + addressZip + "," + firstName + "," + email;
+                    updatedResult = true;
+                }
+
+                updatedDatabase.append(line).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        if (updatedResult) {
+            try (FileWriter writer = new FileWriter(accountDatabase, false)) {
+                writer.write(updatedDatabase.toString());
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        return updatedResult;
     }
+
 }
