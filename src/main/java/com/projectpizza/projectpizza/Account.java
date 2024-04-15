3a235683debd6a47032132f4f2c6b5f92913f4a8
ColinCPittman <colincpittman@gmail.com>
2024-04-15 15:39:27 -0400

    working on login logic, however a bit tricky without all the screens and fields ready
    Signed-off-by: ColinCPittman <colincpittman@gmail.com>

--- src/main/java/com/projectpizza/projectpizza/Account.java
+++ src/main/java/com/projectpizza/projectpizza/Account.java
@@ -13,11 +13,26 @@ public class Account {
     }
 
 
-    public static boolean addAccountToDatabase(String firstName, String lastName, String password, String addressStreet, String addressCity, String addressState, int addressZip, String phone, String email) {
+    /**
+     * Takes in all the account parameters and appends it to the accountDatabase file in a new line.
+     *
+     * @param phone
+     * @param password
+     * @param firstName
+     * @param lastName
+     * @param addressStreet
+     * @param addressCity
+     * @param addressState
+     * @param addressZip
+     * @param email
+     * @return true if operation is successful.
+     */
+    public static boolean addAccountToDatabase(String phone, String password, String accountType, String firstName, String lastName, String addressStreet, String addressCity, String addressState, int addressZip, String email) {
         if (accountDatabase != null) {
             try (FileWriter writer = new FileWriter(accountDatabase, true)) {
                 writer.append(phone + ","
                         + password + ","
+                        + accountType + ","
                         + lastName + ","
                         + addressStreet + ","
                         + addressCity + ","
@@ -33,17 +48,33 @@ public class Account {
         }
         return false;
     }
-    public static boolean login(String phone, String password) {
+
+
+    /**
+     * Takes in the phone and password login credentials and scans the accountDatabase text file for a match.
+     *
+     * @param phone
+     * @param password
+     * @return accountType if login was successful, else "Invalid Password", "Account Does Not Exist", or "Error".
+     */
+    public static String login(String phone, String password) {
         try (Scanner scanner = new Scanner(accountDatabase)) {
             while (scanner.hasNextLine()) {
                 String[] data = scanner.nextLine().split(",");
-                if (data[0].equals(phone) && data[1].equals(password)) {
-                    return true;
-                }
+                if (data[0].equals(phone)) {
+                    if (data[1].equals(password)) {
+                        return data[2];
+                    }
+                    else return "Invalid Password";
+                    }
             }
+            return "Account Does Not Exist";
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-        return false;
+        return "Error";
+    }
+    public static boolean updateAccount(String phone, String password, String accountType, String firstName, String lastName, String addressStreet, String addressCity, String addressState, int addressZip, String email){
+        return true;
     }
 }
--- src/main/java/com/projectpizza/projectpizza/LoginPageController.java
+++ src/main/java/com/projectpizza/projectpizza/LoginPageController.java
@@ -7,11 +7,16 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginPageController {
+
+    public TextField phoneInput;
+    public PasswordField passwordInput;
 
     @FXML
     protected void goToHomePage(ActionEvent event) {
@@ -39,5 +44,22 @@ public class LoginPageController {
             e.printStackTrace();
         }
     }
+    public static void loginController(String phone, String password) {
+        String loginResult = Account.login(phone, password);
+        switch (loginResult) {
+            case "Customer" -> {
 
+            }
+            case "Employee" -> {
+
+            }
+            case "Manager" -> {
+
+            }
+        }
+    }
+
+    public void onLoginPress(ActionEvent actionEvent) {
+        loginController(phoneInput.getText(), passwordInput.getText());
+    }
 }
