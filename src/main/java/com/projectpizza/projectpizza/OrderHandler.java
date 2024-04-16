a921b14bc374bb0fb215c311371e551de962b170
ColinCPittman <colincpittman@gmail.com>
2024-04-16 13:31:10 -0400

    Coded out the toString methods to make it easy to display orders with all their menu items and toppings
    Signed-off-by: ColinCPittman <colincpittman@gmail.com>

--- src/main/java/com/projectpizza/projectpizza/OrderHandler.java
+++ src/main/java/com/projectpizza/projectpizza/OrderHandler.java
@@ -1,8 +1,26 @@
 package com.projectpizza.projectpizza;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class OrderHandler {
-    public boolean createOrder(){
-        //this will instantiate menu items from the fields in the controller and write them to the ordersDatabase file
+    private static final File orderDatabase = new File("orderDatabase.txt");
+    public static boolean createOrder(){
+        //this could be a method that takes in parameters from the MenuPageController that reads the selected options
+        //and instantiates everything, not sure yet until I can see how the menu page is handled
         return true;
+    }
+    public static boolean writeOrderToDatabase(Order order){
+        if (orderDatabase != null) {
+            try (FileWriter writer = new FileWriter(orderDatabase, true)) {
+                writer.append("" + order);
+                return true;
+            } catch (IOException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+        return false;
     }
 }
