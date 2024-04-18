2b188a4831631eb506823fa76fc3c479957eda93
ColinCPittman <colincpittman@gmail.com>
2024-04-18 12:14:11 -0400

    implemented getting and updating logic for totals
    Signed-off-by: ColinCPittman <colincpittman@gmail.com>

--- src/main/java/com/projectpizza/projectpizza/Order.java
+++ src/main/java/com/projectpizza/projectpizza/Order.java
@@ -3,6 +3,26 @@ package com.projectpizza.projectpizza;
 import java.util.ArrayList;
 
 public class Order {
+    private int total;
+
+    public String getTotal() {
+        int dollars = total/100;
+        int cents = dollars % total;
+        return dollars + "." + cents;
+    }
+
+    public void updateTotal(int total) {
+        for (MenuItem item : orderItems ) {
+            total += item.price;
+            if (!item.toppings.isEmpty()) {
+            for (Topping topping : item.toppings) {
+                total += 50;
+            }
+            }
+        }
+    }
+
+    public ArrayList<MenuItem> orderItems = new ArrayList<>();
     @Override
     public String toString() {
         StringBuilder orderDetails = new StringBuilder();
@@ -15,5 +35,4 @@ public class Order {
     public Order() {
     }
 
-    public ArrayList<MenuItem> orderItems = new ArrayList<>();
 }
