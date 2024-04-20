4438d59c47d0872159e3d75198cb48afb800ac31
LoganArnott <larnott1@students.kennesaw.edu>
2024-04-19 22:04:43 -0400

    Can now log in based on account database

--- src/main/java/com/projectpizza/projectpizza/LoginPageController.java
+++ src/main/java/com/projectpizza/projectpizza/LoginPageController.java
@@ -11,12 +11,19 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class LoginPageController {
 
+    @FXML
     public TextField phoneInput;
+    @FXML
     public PasswordField passwordInput;
+
+    private static final File accountDatabase = new File("accountDatabase.txt");
 
     @FXML
     protected void goToNewAccountPage(ActionEvent event) {
@@ -43,5 +50,31 @@ public class LoginPageController {
         }catch(IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public void logIn(ActionEvent event) {
+        try(Scanner sc = new Scanner(accountDatabase)) {
+            String line;
+            String[] split;
+            while(sc.hasNextLine()) {
+                line = sc.nextLine();
+                split = line.split(",");
+                if((phoneInput.getText().toString().equals(split[0])) && (passwordInput.getText().toString().equals(split[1]))) {
+                    try{
+                        Parent textEditorSceneRoot = FXMLLoader.load(getClass().getResource("menu-page.fxml"));
+                        Scene textEditorScene = new Scene(textEditorSceneRoot);
+
+                        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+                        stage.setScene(textEditorScene);
+                        stage.show();
+                    }catch(IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        } catch (FileNotFoundException e) {
+        }
+
     }
 }
